3189fc08f9bbbb1502881918d90c123c24ad8b22
Rubem <rubekx@gmail.com>
2021-06-15 14:49:09 -0300

    Adding new box's colors

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,84 +14,81 @@
             </div>
         </div> --}}
         <div class="flex flex-wrap">
-            <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                 <!--Metric Card-->
-                <div class="bg-white border rounded shadow p-2">
+                <div class="bg-white border rounded p-5 bg-ost-blue" >
                     <div class="flex flex-row items-center">
                         <div class="flex-shrink pr-4">
-                            <div class="rounded p-3 bg-green-600"><i class="fa fa-wallet fa-2x fa-fw fa-inverse"></i>
+                            <div class="rounded p-3 bg-ost-blue"><i class="fa fa-wallet fa-2x fa-fw fa-inverse"></i>
                             </div>
                         </div>
                         <div class="flex-1 text-right md:text-center">
-                            <h5 class="font-bold uppercase text-gray-500">Criado / Created</h5>
-                            <h3 class="font-bold text-3xl">X <span class="text-green-500"><i
-                                        class="fas fa-caret-up"></i></span></h3>
+                            <h5 class="font-bold uppercase text-white">Criado</h5>
+                            <h3 class="font-bold text-4xl text-white">X</h3>
                         </div>
                     </div>
                 </div>
                 <!--/Metric Card-->
             </div>
-            <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                 <!--Metric Card-->
-                <div class="bg-white border rounded shadow p-2">
+                <div class="bg-white border rounded p-5 bg-ost-green">
                     <div class="flex flex-row items-center">
                         <div class="flex-shrink pr-4">
-                            <div class="rounded p-3 bg-pink-600"><i class="fas fa-users fa-2x fa-fw fa-inverse"></i>
+                            <div class="rounded p-3 bg-ost-green"><i class="fas fa-users fa-2x fa-fw fa-inverse"></i>
                             </div>
                         </div>
                         <div class="flex-1 text-right md:text-center">
-                            <h5 class="font-bold uppercase text-gray-500">Fechado / Closed</h5>
-                            <h3 class="font-bold text-3xl">X <span class="text-pink-500"><i
-                                        class="fas fa-exchange-alt"></i></span></h3>
+                            <h5 class="font-bold uppercase text-white">Fechado</h5>
+                            <h3 class="font-bold text-4xl text-white">X</h3>
                         </div>
                     </div>
                 </div>
                 <!--/Metric Card-->
             </div>
-            <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                 <!--Metric Card-->
-                <div class="bg-white border rounded shadow p-2">
+                <div class="bg-white border rounded bg-gray-400 p-5">
                     <div class="flex flex-row items-center">
                         <div class="flex-shrink pr-4">
-                            <div class="rounded p-3 bg-yellow-600"><i
+                            <div class="rounded p-3 bg-gray-400"><i
                                     class="fas fa-user-plus fa-2x fa-fw fa-inverse"></i></div>
                         </div>
                         <div class="flex-1 text-right md:text-center">
-                            <h5 class="font-bold uppercase text-gray-500">Reaberto / Reopened</h5>
-                            <h3 class="font-bold text-3xl">X <span class="text-yellow-600"><i
-                                        class="fas fa-caret-up"></i></span></h3>
+                            <h5 class="font-bold uppercase text-white">Reaberto</h5>
+                            <h3 class="font-bold text-4xl text-white">X</h3>
                         </div>
                     </div>
                 </div>
                 <!--/Metric Card-->
             </div>
-            <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                 <!--Metric Card-->
-                <div class="bg-white border rounded shadow p-2">
+                <div class="bg-white border rounded bg-ost-yelow p-5">
                     <div class="flex flex-row items-center">
                         <div class="flex-shrink pr-4">
-                            <div class="rounded p-3 bg-blue-600"><i class="fas fa-server fa-2x fa-fw fa-inverse"></i>
+                            <div class="rounded p-3 bg-ost-yelow"><i class="fas fa-server fa-2x fa-fw fa-inverse"></i>
                             </div>
                         </div>
                         <div class="flex-1 text-right md:text-center">
-                            <h5 class="font-bold uppercase text-gray-500">Transferido / Transferred</h5>
-                            <h3 class="font-bold text-3xl">X</h3>
+                            <h5 class="font-bold uppercase text-white">Transferido</h5>
+                            <h3 class="font-bold text-4xl text-white">X</h3>
                         </div>
                     </div>
                 </div>
                 <!--/Metric Card-->
             </div>
-            <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+            <div class="w-full md:w-1/2 xl:w-1/3 p-6">
                 <!--Metric Card-->
-                <div class="bg-white border rounded shadow p-2">
+                <div class="bg-white border rounded bg-ost-purple p-5">
                     <div class="flex flex-row items-center">
                         <div class="flex-shrink pr-4">
-                            <div class="rounded p-3 bg-indigo-600"><i class="fas fa-tasks fa-2x fa-fw fa-inverse"></i>
+                            <div class="rounded p-3 bg-ost-purple"><i class="fas fa-tasks fa-2x fa-fw fa-inverse"></i>
                             </div>
                         </div>
                         <div class="flex-1 text-right md:text-center">
-                            <h5 class="font-bold uppercase text-gray-500">Atrasado / Overdue</h5>
-                            <h3 class="font-bold text-3xl">X</h3>
+                            <h5 class="font-bold uppercase text-white">Atrasado</h5>
+                            <h3 class="font-bold text-4xl text-white">X</h3>
                         </div>
                     </div>
                 </div>
