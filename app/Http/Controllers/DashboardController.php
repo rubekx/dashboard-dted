ca280e9ec11a4c7136b1d9c5ab187e50db805272
Rubem Silva <rubekx@gmail.com>
2021-07-30 14:05:24 -0300

    improving methods to load and show ticket data from tockets

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -346,125 +346,162 @@ class DashboardController extends Controller
         return DB::connection('mysql2')->select($sql);
     }
 
+    /**
+     * 230226
+     * Solicitante - Solicitante
+     * Staff - legenda (secretario / tecnico dted)
+     * Detalhe - De "fulano' atribuido para 'outro fulano"
+     * Adicionar caixa criada pelo sistema com status (Ex: Marcado em atraso)
+     * Data no formato BR e remover segundo
+     * listar o polo do solicitante
+     * diponibilizar anexos dos chamados
+     * SELECT `id`, `form_id`, `object_id`, `object_type`, `sort`, `extra`, `created`, `updated` FROM `ost_form_entry` WHERE `object_id` = $thread_id ORDER BY `object_id` DESC
+     * SELECT `entry_id`, `field_id`, `value`, `value_id` FROM `ost_form_entry_values` WHERE `entry_id` = 4569 ORDER BY `entry_id` DESC
+     *
+     */
+
+    public function getThreadHeader($thread_id)
+    {
+        $response = null;
+        if (!empty($thread_id)) {
+            $sql = "SELECT
+                ost_form_entry_values.field_id,
+                ost_form_field.form_id,
+                ost_form_field.label,
+                ost_form_entry_values.value
+                FROM ost_form_entry
+                JOIN ost_form_entry_values ON ost_form_entry_values.entry_id = ost_form_entry.id
+                JOIN ost_form_field ON ost_form_field.id=ost_form_entry_values.field_id
+                WHERE
+                ost_form_entry.form_id = 2
+                AND
+                ost_form_entry.object_id = '" . $thread_id . "'";
+            $result = DB::connection('mysql2')->select($sql);
+            $response = [];
+            foreach ($result as $res) {
+                $response[] = [
+                    'field_id' => $res->field_id,
+                    'label' => $res->label,
+                    'value' => $res->value,
+                    'tipo' => 0,
+                ];
+            }
+        }
+        return $response;
+    }
+    public function getThreadEntries($thread_id, $response)
+    {
+        if (!empty($thread_id)) {
+            $sql = "SELECT poster, body, title, CASE WHEN staff_id != 0 THEN 'Staff' ELSE 'Solicitante' END AS ator, created AS data_post FROM ost_thread_entry WHERE thread_id = '" . $thread_id . "' ORDER BY thread_id ASC";
+            $result = DB::connection('mysql2')->select($sql);
+            $response = [];
+            foreach ($result as $res) {
+                $response[] = [
+                    'data_envio' => $res->data_post,
+                    'autor' => $res->poster,
+                    'tipo_autor' => $res->ator,
+                    'message' => $res->body,
+                    'tipo' => 1,
+                    'title' => $res->title,
+                ];
+            }
+        }
+        return $response;
+    }
+    public function getThreadEnvents($thread_id, $response)
+    {
+        if (!empty($thread_id)) {
+            $sql = "SELECT ost_thread_event.staff_id,
+            ost_thread_event.team_id,
+            ost_thread_event.dept_id,
+            CASE
+                WHEN ost_thread_event.uid IS NULL AND ost_thread_event.state ='reopened' THEN ost_thread_event.username
+                WHEN ost_thread_event.username='SYSTEM' THEN 'Sistema'
+                WHEN ost_thread_event.uid_type='S' THEN CONCAT(ost_staff.firstname,' ',ost_staff.lastname)
+                WHEN ost_thread_event.uid_type='U' THEN ost_user.name
+            END as autor,
+            CASE
+                WHEN ost_thread_event.state = 'closed' THEN 'fechou'
+                WHEN ost_thread_event.state = 'transferred' THEN CONCAT('transferiu para',' <b>', ost_department.name,'</b>')
+                WHEN ost_thread_event.state = 'reopened' THEN 'reabriu '
+                WHEN ost_thread_event.state = 'overdue' THEN 'sinalizou como atrasado'
+                WHEN ost_thread_event.state = 'resent' THEN 'reenviou'
+                WHEN ost_thread_event.state = 'edited' THEN 'editou'
+                WHEN ost_thread_event.state = 'assigned' AND ost_thread_event.data LIKE '%team%' THEN CONCAT('atribuiu isto a',' <b>',ost_team.name,'</b>')
+                WHEN ost_thread_event.state = 'assigned' AND ost_thread_event.data NOT LIKE '%team%' THEN CONCAT('atribuiu isto a',' <b>',staff.firstname,' ',staff.lastname,'</b>')
+                WHEN ost_thread_event.state = 'created' THEN 'criou'
+                ELSE ost_thread_event.state
+            END as status_evento,
+            CASE
+                WHEN ost_thread_event.uid IS NULL AND ost_thread_event.state ='reopened' THEN 'Solicitante'
+                WHEN ost_thread_event.uid_type = 'S' THEN 'Staff'
+                WHEN ost_thread_event.uid_type = 'U' THEN 'Solicitante'
+            END as tipo_autor,
+            ost_thread_event.uid,
+            ost_thread_event.timestamp AS data_post,
+            ost_thread_event.username
+            FROM ost_thread_event
+            LEFT JOIN ost_department ON ost_department.id=ost_thread_event.dept_id AND ost_thread_event.dept_id != 0
+            LEFT JOIN ost_team ON ost_team.team_id=ost_thread_event.team_id AND ost_thread_event.team_id != 0
+            LEFT JOIN ost_staff ON ost_staff.staff_id = ost_thread_event.uid AND ost_thread_event.uid_type='S'
+            LEFT JOIN ost_staff AS staff ON staff.staff_id = ost_thread_event.staff_id AND ost_thread_event.staff_id != 0 AND ost_thread_event.state = 'assigned'
+            LEFT JOIN ost_user ON ost_user.id = ost_thread_event.uid AND (ost_thread_event.uid_type='U' OR (ost_thread_event.state ='reopened' AND ost_thread_event.uid IS NULL))
+            WHERE ost_thread_event.thread_id = '" . $thread_id . "'
+            ORDER BY ost_thread_event.thread_id ASC";
+
+            $result = DB::connection('mysql2')->select($sql);
+            foreach ($result as $res) {
+                $response[] = [
+                    'data_envio' => $res->data_post,
+                    'autor' => $res->autor,
+                    'status' => $res->status_evento,
+                    'tipo_autor' => $res->tipo_autor,
+                    'message' => $res->status_evento,
+                    'tipo' => 2,
+                ];
+            }
+        }
+        return $response;
+    }
+
+    public function eventCard($message, $res)
+    {
+        $data_br = date('d/m/Y H:i:s', strtotime($res['data_envio']));
+        $tipo = $res['tipo'] == 1 ? 'postou' : $res['status'];
+        $color = $res['tipo_autor'] == "Staff" ? 'secondary' : 'primary';
+        $message = $message . '<div class="card" style="margin-bottom:10px;"><div class="card-header bg-' . $color . ' text-white">' . $res['tipo_autor'] . ': <b>' . $res['autor'] . '</b> ' . $res['message'] . ' ' . $data_br . '</div></div>';
+        return $message;
+    }
+
+    public function entryCard($message, $res)
+    {
+        $data_br = date('d/m/Y H:i:s', strtotime($res['data_envio']));
+        $tipo = $res['tipo'] == 1 ? 'postou' : $res['status'];
+        $color = $res['tipo_autor'] == "Staff" ? 'secondary' : 'primary';
+        $title = $res['title'] != null ? $res['title'] : '';
+        $message = $message . '<div class="card" style="margin-bottom:10px;"><div class="card-header bg-' . $color . ' text-white">' . $res['tipo_autor'] . ': <b>' . $res['autor'] . '</b> ' . $tipo . ' ' . $data_br . '  ' . $title . '</div>' .
+            '<div class="card-body"><p class="card-text">' . $res['message'] . '</p></div></div></div>';
+        return $message;
+    }
+
     public function threadEntryAjax($thread_id)
     {
-        $sql = "SELECT
-        poster,
-        body,
-        title,
-        CASE
-            WHEN staff_id != 0 THEN 'Staff'
-            ELSE 'Solicitante'
-        END AS ator,
-        created AS data_post
-        FROM ost_thread_entry
-        WHERE thread_id = '" . $thread_id . "' ORDER BY thread_id ASC";
-
-        $result = DB::connection('mysql2')->select($sql);
-
-        $thread_data = [];
-        foreach ($result as $res) {
-            $thread_data[] = [
-                'data_envio' => $res->data_post,
-                'autor' => $res->poster,
-                'tipo_autor' => $res->ator,
-                'message' => $res->body,
-                'tipo' => 1,
-                // 'status' => null,
-                // 'transferido' => null,
-                'title' => $res->title,
-            ];
-        }
-
-        $sql = "SELECT
-        ost_thread_event.staff_id,
-        ost_thread_event.team_id,
-        ost_thread_event.dept_id,
-        CASE
-            WHEN ost_thread_event.uid IS NULL AND ost_thread_event.state ='reopened' THEN ost_thread_event.username
-            WHEN ost_thread_event.username='SYSTEM' THEN 'Sistema'
-            WHEN ost_thread_event.uid_type='S' THEN CONCAT(ost_staff.firstname,' ',ost_staff.lastname)
-            WHEN ost_thread_event.uid_type='U' THEN ost_user.name
-        END as autor,
-        CASE
-            WHEN ost_thread_event.state = 'closed' THEN 'fechou'
-            WHEN ost_thread_event.state = 'transferred' THEN CONCAT('transferiu para',' <b>', ost_department.name,'</b>')
-            WHEN ost_thread_event.state = 'reopened' THEN 'reabriu '
-            WHEN ost_thread_event.state = 'overdue' THEN 'sinalizou como atrasado'
-            WHEN ost_thread_event.state = 'resent' THEN 'reenviou'
-            WHEN ost_thread_event.state = 'edited' THEN 'editou'
-            WHEN ost_thread_event.state = 'assigned' AND ost_thread_event.data LIKE '%team%' THEN CONCAT('atribuiu isto a',' <b>',ost_team.name,'</b>')
-            WHEN ost_thread_event.state = 'assigned' AND ost_thread_event.data NOT LIKE '%team%' THEN CONCAT('atribuiu isto a',' <b>',staff.firstname,' ',staff.lastname,'</b>')
-            WHEN ost_thread_event.state = 'created' THEN 'criou'
-            ELSE ost_thread_event.state
-        END as status_evento,
-        CASE
-            WHEN ost_thread_event.uid IS NULL AND ost_thread_event.state ='reopened' THEN 'Solicitante'
-            WHEN ost_thread_event.uid_type = 'S' THEN 'Staff'
-            WHEN ost_thread_event.uid_type = 'U' THEN 'Solicitante'
-        END as tipo_autor,
-        ost_thread_event.uid,
-        ost_thread_event.timestamp AS data_post,
-        ost_thread_event.username
-        FROM ost_thread_event
-        LEFT JOIN ost_department ON ost_department.id=ost_thread_event.dept_id AND ost_thread_event.dept_id != 0
-        LEFT JOIN ost_team ON ost_team.team_id=ost_thread_event.team_id AND ost_thread_event.team_id != 0
-        LEFT JOIN ost_staff ON ost_staff.staff_id = ost_thread_event.uid AND ost_thread_event.uid_type='S'
-        LEFT JOIN ost_staff AS staff ON staff.staff_id = ost_thread_event.staff_id AND ost_thread_event.staff_id != 0 AND ost_thread_event.state = 'assigned'
-        LEFT JOIN ost_user ON ost_user.id = ost_thread_event.uid AND (ost_thread_event.uid_type='U' OR (ost_thread_event.state ='reopened' AND ost_thread_event.uid IS NULL))
-        WHERE ost_thread_event.thread_id = '" . $thread_id . "'
-        ORDER BY ost_thread_event.thread_id ASC";
-
-        $result = DB::connection('mysql2')->select($sql);
-
-        foreach ($result as $res) {
-            $thread_data[] = [
-                'data_envio' => $res->data_post,
-                'autor' => $res->autor,
-                'status' => $res->status_evento,
-                'tipo_autor' => $res->tipo_autor,
-                'message' => $res->status_evento,
-                'tipo' => 2,
-            ];
-        }
+        $thread_data[] = [];
+        $thread_data = $this->getThreadEntries($thread_id, $thread_data);
+        $thread_data = $this->getThreadEnvents($thread_id, $thread_data);
 
         usort($thread_data, [$this, 'date_compare']);
+
         // $thread_data = json_encode($thread_data);
         // return $thread_data;
 
         $response = '';
         foreach ($thread_data as $res) {
-
-            $color = $res['tipo_autor'] == "Staff" ? 'secondary' : 'primary';
-            $tipo = $res['tipo'] == 1 ? 'postou' : $res['status'];
-            $data_br = date('d/m/Y H:i:s', strtotime($res['data_envio']));
-           
-            if ($res['tipo'] == 1) { 
-                $title = $res['title'] != null ? $res['title'] : '';
-                $response = $response . '<div class="card" style="margin-bottom:10px;">
-                                            <div class="card-header bg-' . $color . ' text-white">' . $res['tipo_autor'] . ': <b>' . $res['autor'] . '</b> ' . $tipo . ' ' . $data_br . '  '.$title.'</div>
-                                                <div class="card-body">
-                                                    <p class="card-text">' . $res['message'] . '</p>
-                                                </div>
-                                            </div>
-                                        </div>';
+            if ($res['tipo'] == 1) {
+                $response = $this->entryCard($response, $res);
             } else {
-                $response = $response . '<div class="card" style="margin-bottom:10px;">
-                                            <div class="card-header bg-' . $color . ' text-white">' . $res['tipo_autor'] . ': <b>' . $res['autor'] . '</b> ' . $res['message'] . ' ' . $data_br . '</div>
-                                        </div>';
+                $response = $this->eventCard($response, $res);
             }
-            /**
-             * Solicitante - Solicitante
-             * Staff - legenda (secretario / tecnico dted)
-             * Detalhe - De "fulano' atribuido para 'outro fulano"
-             * Adicionar caixa criada pelo sistema com status (Ex: Marcado em atraso)
-             * Data no formato BR e remover segundo
-             * listar o polo do solicitante
-             * diponibilizar anexos dos chamados
-             * SELECT `id`, `form_id`, `object_id`, `object_type`, `sort`, `extra`, `created`, `updated` FROM `ost_form_entry` WHERE `object_id` = $thread_id ORDER BY `object_id` DESC 
-             * SELECT `entry_id`, `field_id`, `value`, `value_id` FROM `ost_form_entry_values` WHERE `entry_id` = 4569 ORDER BY `entry_id` DESC
-             *
-             */
         }
 
         return $response;
@@ -497,7 +534,6 @@ class DashboardController extends Controller
                 ->addIndexColumn()
                 ->addColumn('action', function ($row) {
                     $btn = '<a data-toggle="tooltip" data-id="' . $row->thread_id . '" data-original-title="Edit" class="edit btn btn-primary btn-sm showMessages">Detalhes</a>';
-                    // $btn = '<button type="button" data-id="' . $row->thread_id . '" class="btn btn-primary" data-toggle="modal" showMessages data-target="#modalThreadEntry">Open modal</button>';
                     return $btn;
                 })
                 ->rawColumns(['action'])
@@ -541,81 +577,3 @@ class DashboardController extends Controller
     }
 
 }
-
-// SELECT ost_ticket.number AS chamado,
-// ost_ticket.ticket_id,
-// ost_user.name AS solicitante,
-// ost_user_email.address AS solicitante_email,
-// ost_user__cdata.phone AS telefone,
-// ost_help_topic.topic AS curso,
-// ost_ticket__cdata.subject AS assunto,
-// ost_thread_event.staff_id,
-// ost_thread_event.dept_id,
-// CASE
-//     WHEN ost_thread_event.state = 'closed' THEN 'Fechado'
-//     WHEN ost_thread_event.state = 'transferred' THEN 'Transferido'
-//     WHEN ost_thread_event.state = 'reopened' THEN 'Reaberto'
-//     WHEN ost_thread_event.state = 'overdue' THEN 'Atrasado'
-//     WHEN ost_thread_event.state = 'resent' THEN 'Reenviado'
-//     WHEN ost_thread_event.state = 'edited' THEN 'Editado'
-//     ELSE ost_thread_event.state
-// END as status_evento,
-// ost_ticket_status.name AS status_chamado,
-// DATE_FORMAT(ost_ticket.lastupdate, '%d/%m/%Y %H:%i:%s') ultima_atualizacao,
-// DATE_FORMAT(ost_ticket.created, '%d/%m/%Y %H:%i:%s') envio
-// FROM ost_thread_event
-//  JOIN ost_thread ON ost_thread.id=ost_thread_event.thread_id
-// JOIN ost_ticket ON ost_ticket.ticket_id=ost_thread.object_id
-//  JOIN ost_ticket__cdata ON ost_ticket__cdata.ticket_id=ost_ticket.ticket_id
-//  JOIN ost_ticket_status ON ost_ticket_status.id=ost_ticket.status_id
-//  JOIN ost_user ON ost_user.id=ost_ticket.user_id
-//  JOIN ost_user__cdata ON ost_user__cdata.user_id=ost_user.id
-//  JOIN ost_user_email ON ost_user_email.user_id=ost_user.id
-//  JOIN ost_help_topic ON ost_help_topic.topic_id=ost_ticket.topic_id
-
-// WHERE
-// ost_thread_event.id IN
-// (SELECT MAX(ost_thread_event.id) FROM ost_thread_event WHERE ost_thread_event.state ='overdue' GROUP BY ost_thread_event.thread_id)
-// AND
-// ost_ticket.status_id IN (1,6)
-//  and ost_thread_event.thread_id not IN (SELECT thread_id from ost_thread_event where ost_thread_event.state ='closed'GROUP BY thread_id ORDER BY id DESC)
-// ORDER BY ost_thread_event.thread_id desc
-
-// SELECT ost_ticket.number AS chamado,
-// ost_ticket.ticket_id,
-// ost_user.name AS solicitante,
-// ost_user_email.address AS solicitante_email,
-// ost_user__cdata.phone AS telefone,
-// ost_help_topic.topic AS curso,
-// ost_ticket__cdata.subject AS assunto,
-// ost_thread_event.staff_id,
-// ost_thread_event.dept_id,
-// CASE
-//     WHEN ost_thread_event.state = 'closed' THEN 'Fechado'
-//     WHEN ost_thread_event.state = 'transferred' THEN 'Transferido'
-//     WHEN ost_thread_event.state = 'reopened' THEN 'Reaberto'
-//     WHEN ost_thread_event.state = 'overdue' THEN 'Atrasado'
-//     WHEN ost_thread_event.state = 'resent' THEN 'Reenviado'
-//     WHEN ost_thread_event.state = 'edited' THEN 'Editado'
-//     ELSE ost_thread_event.state
-// END as status_evento,
-// ost_ticket_status.name AS status_chamado,
-// ost_ticket.lastupdate AS ultima_atualizacao,
-// ost_ticket.created AS envio
-// FROM ost_thread_event
-//  JOIN ost_thread ON ost_thread.id=ost_thread_event.thread_id
-// JOIN ost_ticket ON ost_ticket.ticket_id=ost_thread.object_id
-//  JOIN ost_ticket__cdata ON ost_ticket__cdata.ticket_id=ost_ticket.ticket_id
-//  JOIN ost_ticket_status ON ost_ticket_status.id=ost_ticket.status_id
-//  JOIN ost_user ON ost_user.id=ost_ticket.user_id
-//  JOIN ost_user__cdata ON ost_user__cdata.user_id=ost_user.id
-//  JOIN ost_user_email ON ost_user_email.user_id=ost_user.id
-//  JOIN ost_help_topic ON ost_help_topic.topic_id=ost_ticket.topic_id
-
-// WHERE
-// ost_thread_event.id IN
-// (SELECT MAX(ost_thread_event.id) FROM ost_thread_event WHERE ost_thread_event.state ='overdue' GROUP BY ost_thread_event.thread_id)
-// AND
-// ost_ticket.status_id IN (1,6)
-//  and ost_thread_event.thread_id not IN (SELECT thread_id from ost_thread_event where ost_thread_event.state ='closed'GROUP BY thread_id ORDER BY id DESC)
-// ORDER BY ost_thread_event.thread_id desc
