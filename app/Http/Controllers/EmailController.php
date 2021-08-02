a65c98c38f14c48697c393020ffda8e6e71637d8
Rubem Silva <rubekx@gmail.com>
2021-08-02 00:53:21 -0300

    adding osticket controller

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\OsTicketController;
 use App\Models\Email;
 
 class EmailController extends Controller
 {
 
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $emails = Email::all();
         $total = $this->countTickets(5);
+        $os_ticket = new OsTicketController;
+        $os_ticket = $os_ticket->actorEmails(5, 2);
+        // $os_ticket='';
         return view('emails.sended')->with([
+            'os_ticket' => $os_ticket,
             'emails' => $emails,
             'total' => $total,
         ]);
@@ -32,7 +42,6 @@ class EmailController extends Controller
     {
         if (!empty($type)) {
             $total_emals = $this->countTickets($type);
-            
         }
     }
 
